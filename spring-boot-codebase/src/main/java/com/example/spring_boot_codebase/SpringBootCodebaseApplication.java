7c0e356b1ab56f14190df17ac3373861bf6d9d50
anushree <anushreepaul0113@gmail.com>
2024-11-20 14:03:56 +0530

    API

---
+++ spring-boot-codebase/src/main/java/com/example/spring_boot_codebase/SpringBootCodebaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_codebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCodebaseApplication.class, args);
+	}
+
+}
---
+++ spring-boot-codebase/src/test/java/com/example/spring_boot_codebase/SpringBootCodebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_codebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCodebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
