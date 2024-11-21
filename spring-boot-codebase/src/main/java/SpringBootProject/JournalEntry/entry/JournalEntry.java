6f73306cf1aec141dd6dccb3859ce864aa7561ad
anushree <anushreepaul0113@gmail.com>
2024-11-22 01:54:08 +0530

    Spring Boot - Made GET, PUT , POST , DELETE API using Spring Boot.

---
+++ spring-boot-codebase/src/main/java/SpringBootProject/JournalEntry/entry/JournalEntry.java
@@ -0,0 +1,33 @@
+package SpringBootProject.JournalEntry.entry;
+
+public class JournalEntry {
+    private long id;
+    private String title;
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
