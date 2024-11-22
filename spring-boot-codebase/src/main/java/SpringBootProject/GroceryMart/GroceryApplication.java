8c37562681fa935d8e95e4d7a30c1367bf607516
anushree <anushreepaul0113@gmail.com>
2024-11-22 22:03:48 +0530

    Spring Boot -  Grocery CRUD operation using Spring Boot in MongoDB.

--- spring-boot-codebase/src/main/java/SpringBootProject/GroceryMart/GroceryApplication.java
+++ spring-boot-codebase/src/main/java/SpringBootProject/GroceryMart/GroceryApplication.java
@@ -3,6 +3,7 @@ package SpringBootProject.GroceryMart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main application
 @SpringBootApplication
 public class GroceryApplication {
     public static void main(String[] args) {
