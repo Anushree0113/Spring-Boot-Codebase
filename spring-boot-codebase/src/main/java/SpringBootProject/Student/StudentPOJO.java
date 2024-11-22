99285909b15510191bb328d30735361aede70cee
anushree <anushreepaul0113@gmail.com>
2024-11-22 23:30:19 +0530

    Spring Boot - Lombok integration.

--- spring-boot-codebase/src/main/java/SpringBootProject/Student/StudentPOJO.java
+++ spring-boot-codebase/src/main/java/SpringBootProject/Student/StudentPOJO.java
@@ -1,41 +1,14 @@
 package SpringBootProject.Student;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "student-entries")
+@Getter
+@Setter
 public class StudentPOJO {
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Id
     private int id;
