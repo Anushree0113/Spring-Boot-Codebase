6c45d51b84ce81f444a402dad7c275e6b9b306dc
anushree <anushreepaul0113@gmail.com>
2024-11-24 00:04:06 +0530

    Spring Boot - Remove project.

--- spring-boot-codebase/src/main/java/SpringBootProject/Student/repository/StudentEntryRepo.java
+++
@@ -1,8 +0,0 @@
-package SpringBootProject.Student.repository;
-
-import SpringBootProject.Student.StudentPOJO;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentEntryRepo extends MongoRepository<StudentPOJO, Integer> {
-
-}
