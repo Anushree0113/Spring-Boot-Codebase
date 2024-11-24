1fd8397a0d8cd447ce90110e8cf575b04791fa02
anushree <anushreepaul0113@gmail.com>
2024-11-24 18:11:50 +0530

    Spring Boot - Added Spring Boot security dependency.

--- spring-boot-codebase/src/main/java/SpringBootProject/configuration/SecurityConfiguration.java
+++ spring-boot-codebase/src/main/java/SpringBootProject/configuration/SecurityConfiguration.java
@@ -2,9 +2,12 @@ package SpringBootProject.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -21,6 +24,11 @@ public class SecurityConfiguration {
                 .and()
                 .formLogin();  // Form-based login
         return http.build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(MongoDatabaseFactory databaseFactory) {
+        return new MongoTransactionManager(databaseFactory);
     }
 
 
