cbd78e9ef26c04f80cba64574071f07d14e59f17
anushree <anushreepaul0113@gmail.com>
2024-11-24 18:11:14 +0530

    Spring Boot - Added Spring Boot security dependency.

--- spring-boot-codebase/src/main/java/SpringBootProject/configuration/SecurityConfiguration.java
+++ spring-boot-codebase/src/main/java/SpringBootProject/configuration/SecurityConfiguration.java
@@ -2,12 +2,9 @@ package SpringBootProject.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -16,20 +13,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain springSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeHttpRequests()
-                .requestMatchers("/users").permitAll()
-                .anyRequest().authenticated()
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers("/users").permitAll()  // Public access to '/users'
+                .anyRequest().authenticated()  // All other requests require authentication
                 .and()
-                .formLogin();
-        return httpSecurity.build();
+                .formLogin();  // Form-based login
+        return http.build();
     }
 
-    @Bean
-    public PlatformTransactionManager getTransactionManager(MongoDatabaseFactory databaseFactory) {
-        return new MongoTransactionManager(databaseFactory);
-    }
 
 
 }
