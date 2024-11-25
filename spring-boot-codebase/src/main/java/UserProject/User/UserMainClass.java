7647d06d433eb2a4c45972b89f4b840e114aad21
anushree <anushreepaul0113@gmail.com>
2024-11-25 23:06:27 +0530

    Spring Boot - Load user by username.

---
+++ spring-boot-codebase/src/main/java/UserProject/User/UserMainClass.java
@@ -0,0 +1,11 @@
+package UserProject.User;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(UserMainClass.class, args);
+    }
+}
